a2cac8f8de5d88e25ae33e8b1272fb3681a0f06d
DrewArg <andresfabbiano5@gmail.com>
2022-12-04 13:42:04 -0300

    paginas creadas

---
+++ pages/cardInfo.php
@@ -0,0 +1,33 @@
+<?php
+require('../layout/_constants.php');
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require('../layout/_metaTags.php') ?>
+    <?php require('../layout/_styleSheet.php') ?>
+    <title>Información Carta</title>
+</head>
+
+<body class="body">
+    <?php require('../layout/_header.php') ?>
+
+    <main>
+        <div class="inspeccionCarta">
+            <div class="informacionCarta">
+
+            </div>
+            <div class="descripcionCarta">
+
+            </div>
+        </div>
+
+    </main>
+    <?php require('../layout/_footer.php') ?>
+    <?php require('../layout/_jsScripts.php') ?>
+</body>
+
+</html>
