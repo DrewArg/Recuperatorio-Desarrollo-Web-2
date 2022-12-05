1034b993e561ad8999b9fbef6c216d15176678f9
DrewArg <andresfabbiano5@gmail.com>
2022-12-04 23:31:26 -0300

    cartas en la tienda se ven ok

--- pages/shop.php
+++ pages/shop.php
@@ -3,8 +3,6 @@ require('../layout/_constants.php');
 require_once('../functions/func_queries.php');
 
 $cards = getCards($connection);
-
-
 ?>
 
 <!DOCTYPE html>
@@ -58,7 +56,44 @@ $cards = getCards($connection);
         foreach ($cards as $card) :
         ?>
             <div class="carta">
-                <?php echo $card['nombre'] ?>
+                <div class="carta__contorno<?php echo $card['tipo'] ?>" id="tipo<?php echo $card['id'] ?>">
+                    <div class="carta__superior">
+                        <div class="carta__superior--tipo" id="tipoCarta<?php echo $card['id'] ?>">
+                            <?php echo $card['tipo'] ?>
+                        </div>
+                        <div class="carta__cartaReal">
+                            <div class="carta__medio">
+                                <div class="carta__medio--nombre" id="nombreCarta<?php echo $card['id'] ?>">
+                                    <?php echo $card['nombre'] ?>
+                                </div>
+                                <div class="carta__medio--imagen" id="imagenCarta<?php echo $card['id'] ?>">
+                                    <img src="../assets/<?php echo $card['imagen'] ?>">
+                                </div>
+                            </div>
+                            <div class="carta__inferior">
+                                <div class="carta__inferior--efecto" id="efectoCarta<?php echo $card['id'] ?>">
+                                    <p>
+                                        <?php echo $card['efecto'] ?>
+                                    </p>
+                                    <p>
+                                        Cuesta <?php echo $card['alimentos'] ?> alimentos bajarla al tablero.
+                                    </p>
+                                </div>
+
+                            </div>
+                            <div class="carta__precio">
+                                <form action="POST">
+                                    <span>AR$<?php echo $card['precio'] ?></span>
+                                    <input type="submit" id="btnMenos<?php echo $card['id'] ?>" class="btnMenos" name="btnMenos" value="-">
+                                    <span class="cantidadRequerida" id="cantidadRequerida<?php echo $card['id'] ?>">0</span>
+                                    <input type="submit" id="btnMas<?php echo $card['id'] ?>" class="btnMas" name="btnMas" value="+">
+                                </form>
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
             </div>
         <?php endforeach ?>
     </main>
